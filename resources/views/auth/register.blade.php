c7a21872a3efe354d914152281e5df99985b907a
Bach2910 <ngoc22003@gmail.com>
2025-05-15 17:46:15 +0700

    task week 5

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,86 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Đăng ký</title>
-</head>
-<style>
-    .form {
-        display: flex;
-        width: 250px;
-        flex-direction: column;
-        gap: 10px;
-        background-color: rgb(199, 141, 180,1);
-        padding: 2.5em;
-        border-radius: 25px;
-        transition: .4s ease-in-out;
-        box-shadow: rgba(0, 0, 0, 0.4) 1px 2px 2px;
-    }
-
-    .form:hover {
-        transform: translateX(-0.5em) translateY(-0.5em);
-        border: 1px solid #171717;
-        box-shadow: 10px 10px 0px #666666;
-    }
-
-    .heading {
-        color: black;
-        padding-bottom: 2em;
-        text-align: center;
-        font-weight: bold;
-    }
-
-    .input {
-        border-radius: 5px;
-        border: 1px solid whitesmoke;
-        background-color: whitesmoke;
-        outline: none;
-        padding: 0.7em;
-        transition: .4s ease-in-out;
-    }
-
-    .input:hover {
-        box-shadow: 6px 6px 0px #969696,
-        -3px -3px 10px rgb(199, 141, 180,1);
-    }
-
-    .input:focus {
-        background: rgb(199, 141, 180,1);
-        box-shadow: inset 2px 5px 10px rgba(0, 0, 0, 0.3);
-    }
-
-    .form .btn {
-        margin-top: 2em;
-        align-self: center;
-        padding: 0.7em;
-        padding-left: 1em;
-        padding-right: 1em;
-        border-radius: 10px;
-        border: none;
-        color: black;
-        transition: .4s ease-in-out;
-        box-shadow: rgba(0, 0, 0, 0.4) 1px 1px 1px;
-    }
-
-    .form .btn:hover {
-        box-shadow: 6px 6px 0px #969696,
-        -3px -3px 10px rgb(199, 141, 180,1);
-        transform: translateX(-0.5em) translateY(-0.5em);
-    }
-
-    .form .btn:active {
-        transition: .2s;
-        transform: translateX(0em) translateY(0em);
-        box-shadow: none;
-    }
-
-    .form-center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-</style>
-<body>
-<h2>Đăng ký tài khoản</h2>
+@extends('layout.account')
+@section('title','Register')
+@section('content')
 @if(session('error'))
     <p style="color:red;">{{ session('error') }}</p>
 @endif
@@ -93,8 +13,8 @@
         <p class="heading">Register</p>
         <input class="input" placeholder="Username" type="text" name="name">
         <input class="input" placeholder="Email" type="text" name="email">
-        <input class="input" placeholder="Password" type="text" name="password">
-        <input class="input" placeholder="re-enter password" type="text" name="password_confirmation">
+        <input class="input" placeholder="Password" type="password" name="password">
+        <input class="input" placeholder="re-enter password" type="password" name="password_confirmation">
         <div class="d-flex gap-3">
             <button class="btn">Submit</button>
             <button class="btn"><a style="text-decoration: none;color:black" href="{{route('login')}}">Sign In</a>
@@ -102,5 +22,4 @@
         </div>
     </form>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/classes/show.blade.php
+++ resources/views/classes/show.blade.php
@@ -5,17 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
-<h2>Danh sách sinh viên của lớp: {{ $classes->name }}</h2>
-
+<h2>Classes: {{ $classes->name }}</h2>
 <section class="add-card page">
-    <ul>
+    <form class="form">
         @foreach ($classes->students as $student)
-            <li>{{ $student->name }}</li>
+            <label for="name" class="label">
+                <span class="title">Name Student</span>
+                <input
+                    class="input-field"
+                    type="text"
+                    name="name"
+                    value="{{$student->name}}"
+                />
+            </label>
         @endforeach
-    </ul>
+    </form?
 </section>
 </body>
 </html>
